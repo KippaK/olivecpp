444bcfad1e045aed56b63eba01f73f69be471f04
KippaK <antti.venetjoki@gmail.com>
2023-05-19 22:51:14 +0300

    Started to implement gradient, TODOs still left

--- include/shapes/shape.h
+++ include/shapes/shape.h
@@ -7,18 +7,30 @@ class Shape
 public:
 
     Shape(int aPosY, int aPosX, uint32_t RGBA);
+    Shape(int aPosY, int aPosX, uint32_t RGBA1, uint32_t RGBA2);
     ~Shape();
 
     int getPosY() const ;
     int getPosX() const ;
     uint32_t getColor() const;
+    uint32_t getColor(float gradientPos) const; // TODO
 
     void setPosY(int aPosY);
     void setPosX(int aPosX);
     void setColor(uint32_t aColor);
+    void setColor(uint32_t aColor1, uint32_t aColor2);
+
+private: 
+
+struct Gradient
+{
+    uint32_t color1;
+    uint32_t color2;
+};
 
 protected:
     int posX;
     int posY;
     uint32_t color;
+    Gradient gradient;
 };
--- src/shapes/shape.cpp
+++ src/shapes/shape.cpp
@@ -5,6 +5,16 @@ Shape::Shape(int aPosY, int aPosX, uint32_t RGBA)
     posX = aPosX;
     posY = aPosY;
     color = RGBA;
+    gradient.color1 = RGBA;
+    gradient.color2 = RGBA;
+}
+
+Shape::Shape(int aPosY, int aPosX, uint32_t RGBA1, uint32_t RGBA2)
+{
+    posX = aPosX;
+    posY = aPosY;
+    gradient.color1 = RGBA1;
+    gradient.color2 = RGBA2;
 }
 
 Shape::~Shape(){}
@@ -24,6 +34,19 @@ uint32_t Shape::getColor() const
     return color;
 }
 
+uint32_t Shape::getColor(float gradientPos) const
+{
+    uint8_t c1[3] = {
+        // TODO
+    };
+    uint8_t c1[3] = {
+        // TODO
+    };
+    int16_t cd[3] = {
+        // TODO
+    };
+}
+
 void Shape::setPosY(int aPosY)
 {
     posY = aPosY;
@@ -37,4 +60,10 @@ void Shape::setPosX(int aPosX)
 void Shape::setColor(uint32_t aColor)
 {
     color = aColor;
+}
+
+void Shape::setColor(uint32_t aColor1, uint32_t aColor2)
+{
+    gradient.color1 = aColor1;
+    gradient.color2 = aColor2;
 }
