8a1b19d1046ebd4fa260d973f180cbe1c06173b7
KippaK <antti.venetjoki@gmail.com>
2023-06-06 07:36:18 +0300

    Added get and set methods

--- include/shapes/triangle.h
+++ include/shapes/triangle.h
@@ -1,16 +1,28 @@
+#include <stdint.h>
+
 class Triangle
 {
 public:
     Triangle();
-    Triangle(int x1, int y1, int x2, int y2, int x3, int y3);
+    Triangle(int aX1, int aY1, int aX2, int aY2, int aX3, int aY3, uint32_t aColor);
     ~Triangle();
 
+    int getX1() const;
+    void setX1(int aX1);
+    int getY1() const;
+    void setY1(int aY1);
+    int getX2() const;
+    void setX2(int aX2);
+    int getY2() const;
+    void setY2(int aY2);
+    int getX3() const;
+    void setX3(int aX3);
+    int getY3() const;
+    void setY3(int aY3);
+    uint32_t getColor() const;
+    void setColor(uint32_t aColor);
+
 private:
-    struct Point
-    {
-        int x;
-        int y;
-    };
-    
-    Point point[3];
+    int x1, y1, x2, y2, x3, y3;
+    uint32_t color;
 };
--- src/shapes/triangle.cpp
+++ src/shapes/triangle.cpp
@@ -1,14 +1,102 @@
 #include "../../include/shapes/triangle.h"
 
-Triangle::Triangle(/* args */)
+Triangle::Triangle()
 {
+    x1 = 0;
+    y1 = 0;
+    x2 = 0;
+    y2 = 0;
+    x3 = 0;
+    y3 = 0;
+    color = 0x00000000;
 }
 
-Triangle::Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
+Triangle::Triangle(int aX1, int aY1, int aX2, int aY2, int aX3, int aY3, uint32_t aColor)
 {
-    
+    x1 = aX1;
+    y1 = aY1;
+    x2 = aX2;
+    y2 = aY2;
+    x3 = aX3;
+    y3 = aY3;
+    color = aColor;
 }
 
 Triangle::~Triangle()
 {
+}
+
+
+
+
+
+
+int Triangle::getX1() const
+{
+    return x1;
+}
+
+void Triangle::setX1(int aX1)
+{
+    x1 = aX1;
+}
+
+int Triangle::getY1() const
+{
+    return y1;
+}
+
+void Triangle::setY1(int aY1)
+{
+    y1 = aY1;
+}
+
+int Triangle::getX2() const
+{
+    return x2;
+}
+
+void Triangle::setX2(int aX2)
+{
+    x2 = aX2;
+}
+
+int Triangle::getY2() const
+{
+    return y2;
+}
+
+void Triangle::setY2(int aY2)
+{
+    y2 = aY2;
+}
+
+int Triangle::getX3() const
+{
+    return x3;
+}
+
+void Triangle::setX3(int aX3)
+{
+    x3 = aX3;
+}
+
+int Triangle::getY3() const
+{
+    return y3;
+}
+
+void Triangle::setY3(int aY3)
+{
+    y3 = aY3;
+}
+
+uint32_t Triangle::getColor() const
+{
+    return color;
+}
+
+void Triangle::setColor(uint32_t aColor)
+{
+    color = aColor;
 }
